a9a7c15a3915deb1d01074effde265620ea25116
Johneryy <johnakintolu4@gmail.com>
2022-07-18 14:23:23 +0100

    .

---
+++ src/Truecaller/data/models/Contact.java
@@ -0,0 +1,8 @@
+package Truecaller.data.models;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String phoneNumber;
+}
